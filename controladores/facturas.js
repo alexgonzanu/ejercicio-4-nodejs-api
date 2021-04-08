7b7e6238801b5e300749ac67928089fb4239e20f
Alejandro Gonzalez <alexgonzanu@gmail.com>
2021-04-08 13:37:51 +0200

    borrar factura

--- controladores/facturas.js
+++ controladores/facturas.js
@@ -1,14 +1,20 @@
-const facturasJSON = require("../facturas.json").facturas;
+let facturasJSON = require("../facturas.json").facturas;
 const { generaError } = require("../utils/errores");
 
 const getFacturas = () => facturasJSON;
 const getFacturaIngreso = () => facturasJSON.filter(factura => factura.tipo === "ingreso");
 const getFacturaGastos = () => facturasJSON.filter(factura => factura.tipo === "gasto");
 const getFactura = id => facturasJSON.find(factura => factura.id === id);
+const deleteFactura = id => {
+  const factura = facturasJSON.find(factura => factura.id === id);
+  facturasJSON = facturasJSON.filter(factura => factura.id !== id);
+  return factura;
+};
 
 module.exports = {
   getFacturas,
   getFacturaIngreso,
   getFacturaGastos,
-  getFactura
+  getFactura,
+  deleteFactura
 };
--- rutas/facturas.js
+++ rutas/facturas.js
@@ -1,14 +1,45 @@
 const express = require("express");
-const { check } = require("express-validator");
+const { check, checkSchema } = require("express-validator");
 
 const router = express.Router();
 const {
-  getFacturas, getFacturaIngreso, getFacturaGastos, getFactura
+  getFacturas, getFacturaIngreso, getFacturaGastos, getFactura, deleteFactura
 } = require("../controladores/facturas");
 const { idNoExisteError } = require("../utils/errores");
 const facturasJSON = require("../facturas.json").facturas;
 
 const existFactura = idFactura => facturasJSON.find(factura => factura.id === +idFactura);
+
+/* const getFacturasSchema = () => {
+  const numero = {
+
+  }
+  const fecha = {
+
+  }
+  const vencimiento = {
+
+  }
+  const concepto = {
+
+  }
+  const base = {
+    isFloat: {
+      errorMessage: "Valor incorrecto, debe ser un numero"
+    }
+  }
+  const tipoIva = {
+    isInt: {
+      errorMessage: "Valor incorrecto, debe ser un numero"
+    }
+  }
+  const tipo = {
+    isIn
+  }
+  const abonada = {
+
+  }
+} */
 
 router.get("/", (req, res, next) => {
   const facturas = getFacturas();
@@ -31,6 +62,16 @@ router.get("/factura/:idFactura",
     }
     const idFactura = +req.params.idFactura;
     res.json(getFactura(idFactura));
+  });
+router.delete("/factura/:idFactura",
+  check("idFactura", "No existe la factura con esa id").custom(existFactura),
+  (req, res, next) => {
+    const errorIdNotExist = idNoExisteError(req);
+    if (errorIdNotExist) {
+      return next(errorIdNotExist);
+    }
+    const idFactura = +req.params.idFactura;
+    res.json(deleteFactura(idFactura));
   });
 
 module.exports = router;
